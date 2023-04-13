3125349d3c13283d371bdd6948477d593613bab7
Daxorn <nardone.sp@gmail.com>
2023-04-13 11:39:36 +0200

    create 'Player' class

--- app/src/main/java/com/grimschitz/mankomania/PlayerLogic/Player.java
+++ app/src/main/java/com/grimschitz/mankomania/PlayerLogic/Player.java
@@ -1,4 +1,36 @@
 package com.grimschitz.mankomania.PlayerLogic;
 
+import com.grimschitz.mankomania.FieldLogic.Field;
+
+import java.util.HashMap;
+
 public class Player {
+
+    private int playerIndex;
+    private String playerSocket;
+    private int money;
+    private Field currentPosition;
+    private HashMap<Share,Integer> shares;
+    private int dices = 0;
+
+    public Player(){
+        money = 1000000;
+        shares = new HashMap<Share,Integer>();
+    }
+
+    public Player(Field initialField, int playerIndex, String playerSocket){
+        money = 1000000;
+        shares = new HashMap<Share,Integer>();
+        this.playerIndex = playerIndex;
+        currentPosition = initialField;
+        this.playerSocket = playerSocket;
+    }
+
+    //------------GETTERS------------------
+    //public int getDices(){return dices;}
+
+    //-------------------------------------
+    //------------SETTERS------------------
+    //public void setDices(int dices){this.dices = dices;}
+    //-------------------------------------
 }
