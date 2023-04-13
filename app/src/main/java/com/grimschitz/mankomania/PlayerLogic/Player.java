5fc2ead4b038f83bd81ad914cfa9b6a8505bb06f
Daxorn <nardone.sp@gmail.com>
2023-04-13 12:23:06 +0200

    implement basic getters/setters for class Player

--- app/src/main/java/com/grimschitz/mankomania/PlayerLogic/Player.java
+++ app/src/main/java/com/grimschitz/mankomania/PlayerLogic/Player.java
@@ -27,10 +27,36 @@ public class Player {
     }
 
     //------------GETTERS------------------
-    //public int getDices(){return dices;}
-
+    public int getDices(){return dices;}
+    public int getPlayerIndex(){return playerIndex;}
+    public String getPlayerSocket(){return playerSocket;}
+    public int getMoney(){return money;}
+    public int getShareAmount(Share name){return shares.get(name);}
+    public Field getCurrentPosition(){return currentPosition;}
     //-------------------------------------
     //------------SETTERS------------------
-    //public void setDices(int dices){this.dices = dices;}
+    public void setDices(int dices){this.dices = dices;}
+    public void setCurrentPosition(Field field){this.currentPosition = field;}
+    public void setPlayerSocket(String playerSocket){this.playerSocket = playerSocket;}
+    public void setPlayerIndex(int playerIndex){this.playerIndex = playerIndex;}
+    public void setMoney(int money){this.money = money;}
+    public void setShares(int hardSteel, int shortCircuit, int dryOil){
+        shares.clear();
+        shares.put(Share.HARD_STEEL_PLC, hardSteel);
+        shares.put(Share.SHORT_CIRCUIT_PLC, shortCircuit);
+        shares.put(Share.DRY_OIL_PLC, dryOil);
+
+    }
     //-------------------------------------
+    //--------MONEY MANAGEMENT-------------
+    public void addMoney(int amount){ money += amount;}
+    public void loseMoney(int amount){ money -= amount;}
+
+    public void payToPlayer(Player player, int amount){
+        money -= amount;
+        player.addMoney(amount);
+    }
+
+    //-------------------------------------
+
 }
