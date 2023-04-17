ccd7e4c92ad9c9fc0e4f128765527fd5f8aa9d46
Daxorn <nardone.sp@gmail.com>
2023-04-17 11:17:00 +0200

    create Load/Start/Lobby Screen

---
+++ app/src/main/java/com/grimschitz/mankomania/Screens/LoadScreen.java
@@ -0,0 +1,16 @@
+package com.grimschitz.mankomania.Screens;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.grimschitz.mankomania.R;
+
+public class LoadScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_load_screen);
+    }
+}
---
+++ app/src/main/java/com/grimschitz/mankomania/Screens/LobbyScreen.java
@@ -0,0 +1,16 @@
+package com.grimschitz.mankomania.Screens;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.grimschitz.mankomania.R;
+
+public class LobbyScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lobby_screen);
+    }
+}
