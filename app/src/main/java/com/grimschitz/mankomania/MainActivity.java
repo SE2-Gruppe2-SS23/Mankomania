757451f813a80566c02638474ec93fcc71a62a06
erminmiro <102598749+erminmiro@users.noreply.github.com>
2023-04-19 22:55:49 +0200

    made a new activity for rolling the dices
    added a new activity and drawables

--- app/src/main/java/com/grimschitz/mankomania/MainActivity.java
+++ app/src/main/java/com/grimschitz/mankomania/MainActivity.java
@@ -2,13 +2,27 @@ package com.grimschitz.mankomania;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button rollButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        rollButton = findViewById(R.id.rollButton);
+        rollButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,RollDiceActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/grimschitz/mankomania/RollDiceActivity.java
@@ -0,0 +1,14 @@
+package com.grimschitz.mankomania;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RollDiceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_roll_dice);
+    }
+}
