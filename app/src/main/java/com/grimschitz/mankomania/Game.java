25c2c00560b28918859d510243398f8f7559d5ae
Daxorn <nardone.sp@gmail.com>
2023-04-22 11:14:30 +0200

    refactor Screen Code to proper structure

--- app/src/main/java/com/grimschitz/mankomania/Game.java
+++ app/src/main/java/com/grimschitz/mankomania/Game.java
@@ -4,27 +4,19 @@ import android.content.Intent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.grimschitz.mankomania.Screens.LoadScreen;
 import com.grimschitz.mankomania.Screens.StartScreen;
 
 public class Game extends AppCompatActivity {
     private static Game instance;
     //private Board board;
 
+    public Game(){}
     public static Game getInstance(){
         if(instance == null){instance = new Game();}
         return instance;
     }
 
 
-    public void create(){
-       // board = new Board();
-        createActivity(new LoadScreen(new StartScreen()));
-    }
 
 
-    public void createActivity(AppCompatActivity nextActivity){
-        Intent nextScreen = new Intent(this,nextActivity.getClass());
-        this.startActivity(nextScreen);
-    }
 }
--- app/src/main/java/com/grimschitz/mankomania/Launchers/Launcher.java
+++ app/src/main/java/com/grimschitz/mankomania/Launchers/Launcher.java
@@ -2,15 +2,25 @@ package com.grimschitz.mankomania.Launchers;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.grimschitz.mankomania.Game;
+import com.grimschitz.mankomania.GlobalAssets;
+import com.grimschitz.mankomania.R;
+import com.grimschitz.mankomania.Screens.LoadScreen;
+import com.grimschitz.mankomania.Screens.StartScreen;
 
 public class Launcher extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Game.getInstance().create();
+        setContentView(R.layout.activity_launcher);
+        GlobalAssets.nextScreen = StartScreen.class;
+        Game mankomania = new Game();
+
+        Intent nextScreen = new Intent(Launcher.this, LoadScreen.class);
+        this.startActivity(nextScreen);
     }
 }
