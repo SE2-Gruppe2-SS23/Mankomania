f931cfe715f21b39b7ad711f167072fcccd7bd15
Daxorn <nardone.sp@gmail.com>
2023-05-02 14:58:22 +0200

    create branch HorseRace logic, and basic implementation of horse race, race tracks and track types

--- app/src/main/java/com/grimschitz/mankomania/HorseRaceLogic/RaceTrack.java
+++ app/src/main/java/com/grimschitz/mankomania/HorseRaceLogic/RaceTrack.java
@@ -1,21 +1,33 @@
 package com.grimschitz.mankomania.HorseRaceLogic;
 
 public class RaceTrack {
+<<<<<<< HEAD
     private Track current;
+=======
+>>>>>>> 195b4de (create branch HorseRace logic, and basic implementation of horse race, race tracks and track types)
    private double prizeMultiplier;
    private double winChanceMultiplier;
 
 
+<<<<<<< HEAD
    public RaceTrack(Track track,double prizeMultiplier, double winChanceMultiplier){
        this.current = track;
+=======
+   public RaceTrack(double prizeMultiplier, double winChanceMultiplier){
+>>>>>>> 195b4de (create branch HorseRace logic, and basic implementation of horse race, race tracks and track types)
        this.prizeMultiplier = prizeMultiplier;
        this.winChanceMultiplier = winChanceMultiplier;
    }
    //-------------GETTER---------------------------------
+<<<<<<< HEAD
 
     public Track getCurrent() {return current;}
     public double getPrizeMultiplier(){return prizeMultiplier;}
     public double getWinChanceMultiplier(){return winChanceMultiplier;}
+=======
+   public double getPrizeMultiplier(){return this.prizeMultiplier;}
+    public double getWinChanceMultiplier(){return this.winChanceMultiplier;}
+>>>>>>> 195b4de (create branch HorseRace logic, and basic implementation of horse race, race tracks and track types)
    //----------------------------------------------------
 
 
