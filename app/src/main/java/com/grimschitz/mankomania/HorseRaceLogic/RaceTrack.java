d1a08fa6de2c2c6bfc680d1bcde523f4e11b1cf6
Simone Nardone <Simone.Nardone@infineon.com>
2023-05-03 12:59:31 +0200

    add horse icon and race background

--- app/src/main/java/com/grimschitz/mankomania/HorseRaceLogic/RaceTrack.java
+++ app/src/main/java/com/grimschitz/mankomania/HorseRaceLogic/RaceTrack.java
@@ -1,26 +1,21 @@
 package com.grimschitz.mankomania.HorseRaceLogic;
 
 public class RaceTrack {
-<<<<<<< HEAD
-    private Track currentTrack;
-=======
->>>>>>> 582ad3a (create branch HorseRace logic, and basic implementation of horse race, race tracks and track types)
+    private Track current;
    private double prizeMultiplier;
    private double winChanceMultiplier;
 
 
-<<<<<<< HEAD
-   public RaceTrack(Track track , double prizeMultiplier, double winChanceMultiplier){
-       this.currentTrack = track;
-=======
-   public RaceTrack(double prizeMultiplier, double winChanceMultiplier){
->>>>>>> 582ad3a (create branch HorseRace logic, and basic implementation of horse race, race tracks and track types)
+   public RaceTrack(Track track,double prizeMultiplier, double winChanceMultiplier){
+       this.current = track;
        this.prizeMultiplier = prizeMultiplier;
        this.winChanceMultiplier = winChanceMultiplier;
    }
    //-------------GETTER---------------------------------
-   public double getPrizeMultiplier(){return this.prizeMultiplier;}
-    public double getWinChanceMultiplier(){return this.winChanceMultiplier;}
+
+    public Track getCurrent() {return current;}
+    public double getPrizeMultiplier(){return prizeMultiplier;}
+    public double getWinChanceMultiplier(){return winChanceMultiplier;}
    //----------------------------------------------------
 
 
