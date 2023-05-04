4148b192f6b34c06cf5a5e0751e1e785f494e80d
Simone Nardone <Simone.Nardone@infineon.com>
2023-05-04 08:42:12 +0200

    set background for horse race screen and starting position of player horses

---
+++ app/src/main/java/com/grimschitz/mankomania/Screens/HorseRaceScreen.java
@@ -0,0 +1,16 @@
+package com.grimschitz.mankomania.Screens;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.grimschitz.mankomania.R;
+
+public class HorseRaceScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_horse_race_screen);
+    }
+}
