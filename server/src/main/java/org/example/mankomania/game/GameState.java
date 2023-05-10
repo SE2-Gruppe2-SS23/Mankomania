a0e3cb8f13e47bdd2c23423835cba269556e31cc
TheDome0 <dostoeby@edu.aau.at>
2023-05-10 15:37:56 +0200

    add preliminary MINIGAME_XXX values
    https://github.com/SE2-Gruppe2-SS23/Mankomania/issues/24

--- server/src/main/java/org/example/mankomania/game/GameState.java
+++ server/src/main/java/org/example/mankomania/game/GameState.java
@@ -1,6 +1,18 @@
 package org.example.mankomania.game;
 
 public enum GameState {
-//    KEEP SYNCHRONIZED WITH CLIENT!!!
-    HELLO, LOBBY_WAITING, LOBBY_READY, GAME_MOVE, GAME_WAIT, GAME_END
+    //    KEEP SYNCHRONIZED WITH CLIENT!!!
+    HELLO, //simple handshake check
+    LOBBY_WAITING, //only for client
+    LOBBY_READY, //only for client
+    GAME_MOVE, //indicate the client has to make a move and to communicate the move to the server
+//    #position#money
+    GAME_WAIT, //indicate the client has to wait for the other players to make their move
+    GAME_END, //one player reached 0$ and the game is over
+    MINIGAME_CASINO,
+    MINIGAME_RACE,
+    MINIGAME_EXCHANGE,
+    MINIGAME_AUCTION
 }
+
+//split game states up if necessary. Message protocol is 'GameState#anyDataAsString'
