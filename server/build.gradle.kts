9c4fd5fec7f72a704bd974729a4494d0140df2f9
Simone Nardone <Simone.Nardone@infineon.com>
2023-05-22 14:39:14 +0200

    gradle hotfix

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -6,8 +6,6 @@ group = "org.example"
 version = "unspecified"
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.9.1"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
     implementation(libs.vavr)
 }
 
