6596092b98865fa2555188530ab7bd4609aca006
Daxorn <nardone.sp@gmail.com>
2023-05-29 14:41:28 +0200

    implement movement logic and improve layout

--- app/src/main/java/com/grimschitz/mankomania/Screens/HorseRace/HorseRaceScreen.java
+++ app/src/main/java/com/grimschitz/mankomania/Screens/HorseRace/HorseRaceScreen.java
@@ -2,13 +2,16 @@ package com.grimschitz.mankomania.Screens;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.media.Image;
+import android.animation.ObjectAnimator;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.grimschitz.mankomania.HorseRaceLogic.HorseRace;
-import com.grimschitz.mankomania.HorseRaceLogic.Track;
 import com.grimschitz.mankomania.PlayerLogic.Player;
 import com.grimschitz.mankomania.R;
 
@@ -19,30 +22,64 @@ public class HorseRaceScreen extends AppCompatActivity {
     public HashMap<Player, ImageView> playerIcons;
     public ImageView p1,p2,p3,p4;
     public TextView goalView;
-    public ArrayList<TextView> movementFields;
+    public ArrayList<TextView> movementFields = new ArrayList<>();
     public HorseRace raceInstance;
+
+    public ImageView currentPlayer;
+    public Button diceThrow;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_horse_race_screen);
         playerIcons = new HashMap<>();
         p1 = findViewById(R.id.imgPlayer1);
-        p2 = findViewById(R.id.imgPlayer2);
-        p3 = findViewById(R.id.imgPlayer3);
+        p2 = findViewById(R.id.imgPlayer3);
+        p3 = findViewById(R.id.imgPlayer4);
         p4 = findViewById(R.id.imgPlayer4);
+        diceThrow = findViewById(R.id.btn_Throw);
         raceInstance = new HorseRace();
         goalView = findViewById(R.id.goalField);
         setMovementFields();
+
+        diceThrow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                moveIcon(p1,5, v);
+//                GlobalAssets.numberOfDices = 1;
+//               createActivity(RollDiceActivity.class);
+            }
+        });
+
+        //moveIcon(currentPlayer,GlobalAssets.diceAmount);
+
     }
 
-    public int getDestinationField(Track track){
-        return 0;
+    public void moveIcon(ImageView icon, int field, View v){
+        ObjectAnimator animation = ObjectAnimator.ofFloat(icon,"translationX",movementFields.get(field).getRight());
+        animation.setDuration(3000);
+        animation.start();
+//        if(isWinner(icon)){
+//            //TODO send info to server
+//        }
+
     }
-    public void moveIcon(ImageView icon, int field){
-        icon.setRight(movementFields.get(field).getRight());
+    public void setCurrentPlayer(ImageView icon){
+        currentPlayer = icon;
     }
     public void setPlayerIcon(Player p, ImageView icon){
         playerIcons.put(p,icon);
+    }
+
+    public void setIconPos(ImageView icon, int field){
+        icon.setRight(movementFields.get(field).getRight());
+    }
+    public int getIconPos(ImageView icon){
+        for (TextView field: movementFields) {
+            if(icon.getRight() == field.getRight()){
+                return movementFields.indexOf(field);
+            }
+        }
+        return 0;
     }
     public ImageView getPlayerIcon(Player p){
         return playerIcons.get(p);
@@ -56,8 +93,15 @@ public class HorseRaceScreen extends AppCompatActivity {
         movementFields.add(findViewById(R.id.field6));
         movementFields.add(findViewById(R.id.field7));
         movementFields.add(findViewById(R.id.field8));
+        movementFields.add(findViewById(R.id.goalField));
     }
     public boolean isWinner(ImageView playerIcon){
         return playerIcon.getRight() > goalView.getLeft();
     }
+
+    public void createActivity(Class nextActivity){
+        Intent nextScreen = new Intent(this,nextActivity);
+        this.startActivity(nextScreen);
+    }
+
 }
