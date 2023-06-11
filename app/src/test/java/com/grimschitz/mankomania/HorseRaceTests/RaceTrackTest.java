2b4327c1b34ea931b87b53c610ef35eb851f6916
Daxorn <nardone.sp@gmail.com>
2023-06-11 16:34:16 +0200

    implement raceTrack tests

---
+++ app/src/test/java/com/grimschitz/mankomania/HorseRaceTests/RaceTrackTest.java
@@ -0,0 +1,32 @@
+package com.grimschitz.mankomania.HorseRaceTests;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import com.grimschitz.mankomania.BoardLogic.Board;
+import com.grimschitz.mankomania.HorseRaceLogic.HorseRace;
+import com.grimschitz.mankomania.HorseRaceLogic.RaceTrack;
+import com.grimschitz.mankomania.HorseRaceLogic.Track;
+import com.grimschitz.mankomania.PlayerLogic.Player;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+public class RaceTrackTest {
+    private static RaceTrack raceTrack;
+    private static Track track;
+    @BeforeAll
+    public static void init(){
+        track = Track.FIRST;
+        raceTrack = new RaceTrack(track,track.prizeMultiplier, track.winMultiplier);
+    }
+
+    @Test
+    public void testNewRaceTrack(){assertNotNull(raceTrack);}
+    @Test
+    public void testGetCurrent(){assertEquals(track,raceTrack.getCurrent());}
+    @Test
+    public void testGetPrizeMultiplier(){assertEquals(track.prizeMultiplier,raceTrack.getPrizeMultiplier());}
+    @Test
+    public void testGetWinMultiplier(){assertEquals(track.winMultiplier,raceTrack.getWinChanceMultiplier());}
+}
