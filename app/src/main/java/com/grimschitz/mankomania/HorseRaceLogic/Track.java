64e614654c8c2edc8160a00562b9821341d38f97
Daxorn <49523357+Daxorn@users.noreply.github.com>
2023-06-18 20:19:42 +0200

    implement more player class tests

--- app/src/main/java/com/grimschitz/mankomania/HorseRaceLogic/Track.java
+++ app/src/main/java/com/grimschitz/mankomania/HorseRaceLogic/Track.java
@@ -1,7 +1,6 @@
 package com.grimschitz.mankomania.HorseRaceLogic;
 
 public enum Track {
-    //TODO modify multipliers accordingly
     FIRST(0.25,2.5),
     SECOND(0.5,2.0),
     THIRD(0.75,1.5),
@@ -11,7 +10,7 @@ public enum Track {
 public final double winMultiplier;
 public final double prizeMultiplier;
 
-private Track(double winMult, double prizeMult){
+Track(double winMult, double prizeMult){
     this.prizeMultiplier = prizeMult;
     this.winMultiplier = winMult;
 }
